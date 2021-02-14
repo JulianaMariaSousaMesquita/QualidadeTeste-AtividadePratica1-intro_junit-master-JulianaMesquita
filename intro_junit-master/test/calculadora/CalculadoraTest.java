a0897492a5f8fc6178bcca28d5020fdd6cfef063
Juliana Mesquita <60270373+JulianaMariaSousaMesquita@users.noreply.github.com>
2021-02-14 18:25:03 -0300

    Continuação de testes da calculadoraTest
    Adicionado novos teste na calculadoraTest:
    - subtração de dois números
    - multiplicação de dois números
    - somatoria
    - ehpositivo com valor positivo (testando dois retornos verdadeiro e falso)
    - ehpositivo com valor negativo (testando dois retornos verdadeiro e falso)
    - comparar dois valores

--- intro_junit-master/test/calculadora/CalculadoraTest.java
+++ intro_junit-master/test/calculadora/CalculadoraTest.java
@@ -1,6 +1,6 @@
 package calculadora;
 
-
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -16,38 +16,87 @@ public class CalculadoraTest {
 	
 	private Calculadora calc;
 	
+	@DisplayName("Inicializando classe para teste da calculadora")
 	@BeforeEach
 	public void inicializa() {
 		calc = new Calculadora();
 	}
 	
-	@DisplayName("Testa a soma de dois n�meros")
+	@DisplayName("Testa a soma de dois números")
 	@Test
 	public void testSomaDoisNumeros() {
 		int soma = calc.soma(4, 5);		
 		Assertions.assertEquals(9, soma);		
 	}
 	
+	@DisplayName("Testa divisão de dois números")
 	@Test
 	public void testDivisaoDoisNumeros() {
 		int divisao = calc.divisao(8, 4);
 		assertTrue(divisao == 2);
 	}
 	
+	@DisplayName("Testa divisão por zero")
 	@Test
 	public void testDivisaoPorZero() {
 		try {
 			int divisao = calc.divisao(8, 0);
-			fail("Exce��o n�o lan�ada");
+			fail("Exceção não lançada");
 		}catch (ArithmeticException e) {
 			assertEquals("/ by zero", e.getMessage());
 		}		
 	}
 	
+	@DisplayName("Testa divisão por zero com Assert Throws")
 	@Test
 	public void testDivisaoPorZeroComAssertThrows() {
 		assertThrows(ArithmeticException.class,
 				() -> calc.divisao(8, 0));
 	}
-
+	
+	@DisplayName("Testa a subtração de dois números")
+	@Test
+	public void testSubtracaoDoisNumeros() {
+		int sub = calc.subtracao(5, 5);		
+		Assertions.assertEquals(0, sub);		
+	}
+	
+	@DisplayName("Testa a multiplicação de dois números")
+	@Test
+	public void testMultiplicacaoDoisNumeros() {
+		int mult = calc.multiplicacao(5, 5);		
+		Assertions.assertEquals(25, mult);		
+	}
+	
+	@DisplayName("Testa a somatoria")
+	@Test
+	public void testSomatoria() {
+		int somatoria = calc.somatoria(5);		
+		Assertions.assertEquals(15, somatoria);		
+	}
+	
+	@DisplayName("Testa ehpositivo") //valor positivo
+	@Test
+	public void testEhpositivoNumeroPositivo() {
+		boolean ehpositivo = calc.ehPositivo(5);		
+		assertTrue(ehpositivo);
+		assertFalse(ehpositivo); //Failures
+		
+	}
+	
+	@DisplayName("Testa ehpositivo") //valor negativo
+	@Test
+	public void testEhpositivoNumeroNegativo() {
+		boolean ehpositivo = calc.ehPositivo(-8);		
+		assertFalse(ehpositivo);
+		assertTrue(ehpositivo); //Failures
+	}
+	
+	
+	@DisplayName("Testa compara")
+	@Test
+	public void testComparaDoisNumeros() {
+		int compara = calc.compara(5, 5);		
+		Assertions.assertEquals(0, compara);	
+	}
 }
