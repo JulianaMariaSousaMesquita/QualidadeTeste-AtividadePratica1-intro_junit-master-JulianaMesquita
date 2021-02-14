256f28b82318593dad0872d09a85cd4d20765ca6
Juliana Mesquita <60270373+JulianaMariaSousaMesquita@users.noreply.github.com>
2021-02-14 18:09:23 -0300

    Atualizar - versão final

--- src/produto/ProdutoNaoEncontradoException.java
+++ src/produto/ProdutoNaoEncontradoException.java
@@ -2,6 +2,11 @@ package produto;
 
 public class ProdutoNaoEncontradoException extends Exception{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ProdutoNaoEncontradoException() {
 		super();
 	}
